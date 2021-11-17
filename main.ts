a98ff951e1dcfa3f89123ffe1c7e92e1d7797fed
svi-berlin <svi-berlin@users.noreply.github.com>
2021-11-17 19:04:36 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,52 +1,25 @@
 input.onButtonPressed(Button.A, function () {
-    if (ordner != 3) {
-        if (sound < 11) {
-            sound += 1
-            serialmp3.runMp3Command(Mp3Command.PLAY_NEXT_TRACK)
-        } else {
-            sound = 1
-            ordner += 1
-            serialmp3.playMp3Track(sound, ordner)
-        }
+    if (position > 30) {
+        position += -20
+        pins.servoWritePin(AnalogPin.P1, position)
     } else {
-        pins.servoWritePin(AnalogPin.P1, 180)
-    }
-})
-input.onButtonPressed(Button.AB, function () {
-    if (ordner == 3) {
-        serialmp3.runMp3Command(Mp3Command.PLAY_NEXT_TRACK)
-        pins.servoWritePin(AnalogPin.P1, 64)
+        position = 90
+        pins.servoWritePin(AnalogPin.P1, position)
     }
 })
 input.onButtonPressed(Button.B, function () {
-    if (ordner != 3) {
-        if (sound < 11) {
-            sound += 1
-            serialmp3.runMp3Command(Mp3Command.PLAY_NEXT_TRACK)
-        } else {
-            sound = 1
-            ordner += 1
-            serialmp3.playMp3Track(sound, ordner)
-        }
+    if (position < 150) {
+        position += 20
+        pins.servoWritePin(AnalogPin.P1, position)
     } else {
-        pins.servoWritePin(AnalogPin.P1, 0)
+        position = 90
+        pins.servoWritePin(AnalogPin.P1, position)
     }
 })
-let ordner = 0
-let sound = 0
-sound = 1
-ordner = 1
+let position = 0
 basic.setLedColor(0x00ff00)
-serialmp3.connectSerialMp3(DigitalPin.C16, DigitalPin.C17)
-serialmp3.playMp3Track(sound, ordner)
-basic.pause(500)
-basic.setLedColor(0x0000ff)
-ordner += 1
-serialmp3.playMp3Track(sound, ordner)
+position = 90
+pins.servoWritePin(AnalogPin.P1, position)
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
-    basic.setLedColor(0xff0000)
-    basic.pause(500)
-    basic.setLedColor(0xffff00)
-    basic.showIcon(IconNames.SmallHeart)
+    basic.showNumber(position)
 })
