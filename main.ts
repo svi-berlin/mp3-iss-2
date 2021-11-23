76c5e70f42b76d5e699ea544c30a1e9f86660be1
svi-berlin <svi-berlin@users.noreply.github.com>
2021-11-23 19:45:55 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,34 @@
 input.onButtonPressed(Button.A, function () {
-    if (position > 30) {
-        position += -20
-        pins.servoWritePin(AnalogPin.P1, position)
-    } else {
-        position = 90
-        pins.servoWritePin(AnalogPin.P1, position)
-    }
+    mp3 += 1
+    basic.showNumber(mp3)
+    serialmp3.playMp3Track(mp3, 1)
+    dreheKopfLinks()
 })
-input.onButtonPressed(Button.B, function () {
-    if (position < 150) {
-        position += 20
+function dreheKopfRechts () {
+    while (position - 2 < 150) {
+        position += 2
         pins.servoWritePin(AnalogPin.P1, position)
-    } else {
-        position = 90
-        pins.servoWritePin(AnalogPin.P1, position)
+        basic.pause(1000)
     }
+    basic.showNumber(position)
+}
+function dreheKopfLinks () {
+    while (position - 2 > 30) {
+        position += -2
+        pins.servoWritePin(AnalogPin.P1, position)
+        basic.pause(1000)
+    }
+    basic.showNumber(position)
+}
+input.onButtonPressed(Button.B, function () {
+    dreheKopfRechts()
 })
 let position = 0
+let mp3 = 0
+mp3 = 0
+serialmp3.connectSerialMp3(DigitalPin.C16, DigitalPin.C17)
 basic.setLedColor(0x00ff00)
-position = 90
-pins.servoWritePin(AnalogPin.P1, position)
+serialmp3.setMp3Volume(15)
 basic.forever(function () {
-    basic.showNumber(position)
+	
 })
