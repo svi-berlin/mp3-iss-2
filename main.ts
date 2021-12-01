aed12203880bc2a080963a036fc8f9a1f497e48f
svi-berlin <svi-berlin@users.noreply.github.com>
2021-12-01 19:50:22 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,40 +1,18 @@
+function starteQuiz () {
+    serialmp3.playMp3TrackFromFolder(1, 2, Mp3Repeat.No)
+}
 input.onButtonPressed(Button.A, function () {
-    mp3 += 1
-    basic.showNumber(mp3)
-    dreheKopfLinks()
+    sprecheIntro()
 })
-function dreheKopfRechts () {
-    while (position - 2 < 150) {
-        position += 2
-        pins.servoWritePin(AnalogPin.P1, position)
-        basic.pause(1000)
-    }
-    basic.showNumber(position)
+function sprecheIntro () {
+    serialmp3.playMp3TrackFromFolder(2, 1, Mp3Repeat.No)
 }
-function dreheKopfLinks () {
-    while (position - 2 > 30) {
-        position += -2
-        pins.servoWritePin(AnalogPin.P1, position)
-        basic.pause(1000)
-    }
-    basic.showNumber(position)
-}
-input.onButtonPressed(Button.AB, function () {
-    mp3 = 1
-    basic.showNumber(mp3)
-    serialmp3.playMp3Track(mp3, 1)
-    pins.servoWritePin(AnalogPin.P1, position)
-})
 input.onButtonPressed(Button.B, function () {
-    mp3 += -1
-    basic.showNumber(mp3)
-    dreheKopfRechts()
+    starteQuiz()
 })
-let position = 0
-let mp3 = 0
-mp3 = 0
 serialmp3.connectSerialMp3(DigitalPin.C16, DigitalPin.C17)
 basic.setLedColor(0xff0000)
-serialmp3.setMp3Volume(15)
-serialmp3.playMp3Track(mp3, 1)
+serialmp3.setMp3Volume(30)
+serialmp3.playMp3Folder(1, Mp3Repeat.No)
+basic.showNumber(1)
 basic.setLedColor(0x00ff00)
