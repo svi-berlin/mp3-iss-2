ddb09779132e8c5e86684bd135c4bf53078b97de
svi-berlin <svi-berlin@users.noreply.github.com>
2021-12-06 18:31:35 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,13 @@ function starteQuiz () {
     quizGestartet = 1
     serialmp3.playMp3TrackFromFolder(frage, quiz, Mp3Repeat.No)
 }
+radio.onReceivedNumber(function (receivedNumber) {
+    pins.servoWritePin(AnalogPin.P1, 20)
+    basic.pause(2000)
+    pins.servoWritePin(AnalogPin.P1, 170)
+    basic.pause(2000)
+    serialmp3.playMp3TrackFromFolder(receivedNumber, 3, Mp3Repeat.No)
+})
 input.onButtonPressed(Button.A, function () {
     pins.servoWritePin(AnalogPin.P1, 10)
     basic.pause(2000)
@@ -34,14 +41,15 @@ input.onButtonPressed(Button.B, function () {
 let frage = 0
 let quiz = 0
 let quizGestartet = 0
+radio.setGroup(1)
+radio.setTransmitPower(1)
 pins.servoWritePin(AnalogPin.P1, 90)
 quizGestartet = 0
 quiz = 2
 frage = 1
 serialmp3.connectSerialMp3(DigitalPin.C16, DigitalPin.C17)
 basic.setLedColor(0x007fff)
-serialmp3.setMp3Volume(30)
-serialmp3.playMp3Folder(1, Mp3Repeat.No)
+serialmp3.setMp3Volume(10)
 basic.showIcon(IconNames.Heart)
 pins.servoWritePin(AnalogPin.P1, 20)
 basic.pause(2000)
@@ -50,3 +58,4 @@ basic.pause(2000)
 pins.servoWritePin(AnalogPin.P1, 170)
 basic.pause(2000)
 pins.servoWritePin(AnalogPin.P1, 32)
+serialmp3.playMp3Folder(1, Mp3Repeat.No)
